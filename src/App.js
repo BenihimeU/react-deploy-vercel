530dc2ea855b1ee950ed1528317e0f3c40552e06
Vishwas HK <v-vishwash@microsoft.com>
2022-09-14 15:29:56 +0530

    Critical fix

--- src/App.js
+++ src/App.js
@@ -2,44 +2,55 @@ import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
-const defaultColor = '#0000FF';
+
+const defaultColor = '#f15025';
 function App() {
-  const [color, setColor] = useState(defaultColor);
-  const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values(color).all(10));
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values(defaultColor).all(10))
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      let colors = new Values(color).all(10);
-      console.log(colors);
-      setList(colors);
-    }
-    catch (ex) {
-      setError(true);
-      console.log(ex);
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
     }
   }
 
-  return <>
-    <section className='container'>
-      <h3>Color Generator</h3>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setColor(e.target.value)}
-          placeholder={defaultColor}
-          className={`${error ? 'error' : ''}`}
-        ></input>
-        <button type='submit' className='btn'>Submit</button>
-      </form>
-    </section>
-    <section className='colors'>
-      {
-        list.map((color, index) => {
-          return <SingleColor key={index} {...color} index={index} />
-        })
-      }
-    </section>
-  </>
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder={defaultColor}
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
